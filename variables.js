f74b0d00b0724f51910ef69689b35e36cb9934b4
Veena Biradar <veenabiradar463@gmail.com>
2025-04-28 13:21:09 +0530

    hoisting concept

---
+++ variables.js
@@ -0,0 +1,25 @@
+// Hoisting in js 
+// we can acces variables and functions we before we initialize it 
+
+// var x = 7;
+// function getName() {
+//     console.log('hello world')
+// }
+
+// getName();
+// console.log(x);
+
+getName();
+console.log(x);
+console.log(getName)
+sum();
+
+var x = 7;
+function getName() {
+    console.log('hello world')
+}
+
+// Arrow function here if i call  sum() before initialization sere sum behaves as variable and initially it is stored as undefined 
+let sum = () => {
+ console.log('hello')
+}
